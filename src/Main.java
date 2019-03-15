8d15807a609cd1002beee6ac6d1bed26121102a0
Nguyễn Ngọc Đạt <nguoikoquen2@gmail.com>
2019-03-15 10:30:33 +0700

    Update Main.java
    Add comment

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,11 @@
  * @author MemeLord
  */
 public class Main {
+    /**
+     * Chú ý: Đây chỉ là hướng làm việc của thuật toán nén LZW,
+     * Nếu về mặt lập trình và ngôn ngữ máy tính thì thuật toán viết ở đây hoàn toàn sai
+     * @param args 
+     */
     public static void main(String[] args) {
         String eg = "ABABABABAB";
         Encoder e = new Encoder();
